97dd81b20a6571969498c69500fece918812d2d5
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-11 19:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let count = 0;
+let element = document.querySelector("#level");
+
+while (element) {
+    count++;
+    element = element.parentElement; 
+}
+
+alert(`The level of the element is: ${count}`);
